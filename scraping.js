daefd32b4561fb42c9400c2023bf7eb59d4c0fcb
Caitlin Keck <caitlin.keck@gmail.com>
2017-10-15 19:15:33 -0400

    first commit

---
+++ scraping.js
@@ -0,0 +1,10 @@
+const cheerio = require('cheerio')
+const $ = cheerio.load('https://www.icoalert.com/')
+
+var urls = []
+
+$('.vsc-initialized .container-main .col-contain-upcoming .col.upcoming .column-count .ico-wrap .ico-links').each(function(index, element){
+  urls.push(element)
+});
+
+console.log(urls)
