bcf8d216695b3b6a787bf343e03d3206c57e2b48
Aaron Vasquez <aaronvasquez1@gmail.com>
2017-10-15 21:22:31 -0400

    setup skeleton for parsing ICO Alert

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-// TODO(catkeck): Start script here.
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-console.log("Hello World...");
+const ICO_ALERT_URL = "https://www.icoalert.com/";
+
+console.log("Fetching ICO Alert...");
+axios.get(ICO_ALERT_URL).then(({ data }) => {
+  console.log("Parsing ICO Alert...");
+  parseIcoAlert(data);
+});
+
+const parseIcoAlert = html => {
+  const $ = cheerio.load(html);
+
+  // TODO(catkeck): Parse out urls from the anchor tags.
+  $(".upcoming .ico-wrap .ico-links").each(console.log);
+};
