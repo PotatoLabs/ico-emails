ebb1bac1260aef6ff933d31f687d5cd809e8b8fd
Caitlin Keck <caitlin.keck@gmail.com>
2017-10-26 18:06:56 -0400

    got it to start listing emails

--- index.js
+++ index.js
@@ -2,14 +2,24 @@
 const request = require('request');
 const cheerio = require("cheerio");
 const $ = cheerio.load("https://www.icoalert.com/");
-
+var urls = []
 request("https://www.icoalert.com/", function (error, response, body) {
   if (!error && response.statusCode == 200) {
     // console.log(html);
     const $ = cheerio.load(body);
-    let links = $('.col-contain-upcoming').find('a')
+    var links = $('.col-contain-upcoming').find('a')
     $(links).each(function(i, link){
-      console.log($(link).attr('href'));
+      urls.push($(link).attr('href'));
     });
   }
+  var emails = []
+  urls.map(function(url){
+    console.log(url)
+    request(url, function (error, response, body){
+      if (!error && response.statusCode == 200) {
+        console.log(body.match(/(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/gi));
+      }
+    })
+  })
+
 });
