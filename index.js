9f9442b532aa2826c6c3e49116dcb38f303f8f59
Caitlin Keck <caitlin.keck@gmail.com>
2017-10-26 16:51:53 -0400

    cleaned up code to only show working parts

--- index.js
+++ index.js
@@ -3,20 +3,6 @@ const request = require('request');
 const cheerio = require("cheerio");
 const $ = cheerio.load("https://www.icoalert.com/");
 
-// let blah = $(element).find('.ico-links')
-// // $(".ico-links").each(function(index, element) {
-// //   urls.push(element);
-// // });
-
-// var urls = $(
-//  ".upcoming .ico-wrap"
-// ).map(function(element) {
-//  console.log(element);
-// });
-
-// console.log(urls);
-
-
 request("https://www.icoalert.com/", function (error, response, body) {
   if (!error && response.statusCode == 200) {
     // console.log(html);
@@ -25,15 +11,5 @@ request("https://www.icoalert.com/", function (error, response, body) {
     $(links).each(function(i, link){
       console.log($(link).attr('href'));
     });
-    // console.log($);
-    // let length = $('.ico-links').children().first().text();
-    // console.log(length)
-    // let text = $('src').filter('.upcoming').attr('class');
-    // // let text = $('.col.upcoming .ico-links').attr("src");
-    // console.log(text);
-    // $('ico-links').each(function(i, element){
-    //   let a = $(this);
-    //   console.log(a.text());
-    // });
   }
 });
