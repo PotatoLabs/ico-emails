3397999f96afb4bdb775918abc2ecf0a6d714a14
Caitlin Keck <caitlin.keck@gmail.com>
2017-10-26 16:09:31 -0400

    changed code to use request and logged html of page

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 // TODO(catkeck): Start script here.
-
+const request = require('request');
 const cheerio = require("cheerio");
 const $ = cheerio.load("https://www.icoalert.com/");
 
-var urls = [];
+// let blah = $(element).find('.ico-links')
+// // $(".ico-links").each(function(index, element) {
+// //   urls.push(element);
+// // });
 
-$(
-  ".vsc-initialized .container-main .col-contain-upcoming .col.upcoming .column-count .ico-wrap .ico-links"
-).each(function(index, element) {
-  urls.push(element);
+// var urls = $(
+//  ".upcoming .ico-wrap"
+// ).map(function(element) {
+//  console.log(element);
+// });
+
+// console.log(urls);
+
+
+request("https://www.icoalert.com/", function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    console.log(html);
+    const $ = cheerio.load(html);
+    $('ico-wrap').each(function(i, element){
+      var a = $(this).prev();
+      console.log(a.text());
+    });
+  }
 });
-
-console.log(urls);
